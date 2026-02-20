607f8744f92b60d4dfc520aa6dc8841e2d70aa4a
Evan <ebogedin@comcast.net>
2026-02-20 14:43:04 -0500

    Add cache clean up

--- sw.js
+++ sw.js
@@ -1,19 +1,36 @@
-const CACHE_NAME = 'count-down-cache-v1';
+const CACHE_NAME = 'count-down-cache-v2';
 const ASSETS = [
   '/',
   '/index.html',
   '/css/style.css',
   '/js/timer.js',
-  '/assets/img/icon.png',
+  '/assets/img/HourGlass192.png',
   '/assets/audio/Chill_Alarm3.mp3'
 ];
 
+// install
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
+// clean up
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// handle requests from cache
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(cached => {
